fed633dd8a4bfc4c8869a30290d69d077087d0bc
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2019-05-30 03:16:43 +0700

    add gTTS API

--- zhres/__init__.py
+++ zhres/__init__.py
@@ -3,12 +3,14 @@ from flask_cors import CORS
 
 from .api.lib import api_lib
 from .api.zh import api_zh
+from .api.tts import api_tts
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="dist")
 CORS(app)
 
 app.register_blueprint(api_lib)
 app.register_blueprint(api_zh)
+app.register_blueprint(api_tts)
 
 @app.route("/")
 def index():
